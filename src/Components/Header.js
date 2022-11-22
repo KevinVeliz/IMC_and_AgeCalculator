ef48c7cdd44fc48a53f9d2ad4f4f81a4491b415e
KevinVeliz <mario-veliz258934@hotmail.com>
2022-11-22 16:24:44 -0500

    IMC terminado

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import '../App.css'
+
+const Header = () => {
+    return ( 
+        <div className='box'>
+            <h1 className='Header'>
+                Índice de Masa Corporal
+            </h1>
+        </div>
+     );
+}
+ 
+export default Header;
